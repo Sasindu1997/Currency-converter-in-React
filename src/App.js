e15ada2e5a2e1d8b2c7b6282529dbdba48c0a6f9
Sasindu1997 <sasinduusgalhewa@gmail.com>
2020-07-07 00:07:54 +0530

    set outout onchaning values

--- src/App.js
+++ src/App.js
@@ -34,6 +34,16 @@ function App() {
     })
   }, [])
 
+  function handleFromAmountChange(e){
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true)
+  }
+
+  function handleToAmountChange(e){
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false)
+  }
+
   return (
     <>
       <h1>Convert</h1>
@@ -41,13 +51,16 @@ function App() {
         currencyOptions={currencyOptions}
         selectedCurrency={fromCurrency}
         onChangeCurrency={e => setFromCurrency(e.target.value)}
-
+        onChangeAmount={handleFromAmountChange}
+        amount = {fromAmount}
       />
       <div className="equals">=</div>
       <CurrencyRow
         currencyOptions={currencyOptions}
         selectedCurrency={toCurrency}
         onChangeCurrency={e => setToCurrency(e.target.value)}
+        onChangeAmount={handleToAmountChange}
+        amount={toAmount}
       />
     </>
   );
